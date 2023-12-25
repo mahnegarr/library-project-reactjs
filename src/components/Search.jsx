685bf534f6526092bf202e73f5ee432b3006d126
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2023-12-25 19:46:48 +0330

    #

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoSearch } from "react-icons/io5";
 
 function Search() {
+  const [search, setSearch] = useState("");
+  const changeHandler =(e)=>{
+    console.log(e.target.value);
+
+  }
   return (
     <div className="searchBox">
-      <input type="search" name="" placeholder="Search title" id="" />
-      <button className="searchIcon">
-        <IoSearch fontSize="1.5rem"/>
+      <input type="search" name="" placeholder="Search title" id="" onChange={changeHandler} />
+      <button className="searchIcon" >
+        <IoSearch fontSize="1.5rem" />
       </button>
     </div>
   );
